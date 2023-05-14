eaf1ac0ca7fc99099574bc9055de8d7bad62ab8a
aki <lala.akira@gmail.com>
2023-05-14 20:52:44 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -4,41 +4,44 @@ import { Selection } from 'vscode';
 const COMMENT_PATTERN = /^\s*<% if false %>\s+<!--.+?-->\s+<% end %>$/ms;
 
 const toggleComment = (text: string, indent: number) => {
-	if (COMMENT_PATTERN.test(text)) {
-		const lines = text.split("\n");
-		return lines.slice(2, -2).join("\n");
-	}
+  if (COMMENT_PATTERN.test(text)) {
+    const lines = text.split("\n");
+    return lines.slice(2, -2).join("\n");
+  }
 
-	const whitespace = ' '.repeat(indent);
-	return [
-		`${whitespace}<% if false %>`,
-		`${whitespace}<!--`,
-		text,
-		`${whitespace}-->`,
-		`${whitespace}<% end %>`,
-	].join("\n");
+  const whitespace = ' '.repeat(indent);
+  return [
+    `${whitespace}<% if false %>`,
+    `${whitespace}<!--`,
+    text,
+    `${whitespace}-->`,
+    `${whitespace}<% end %>`,
+  ].join("\n");
 };
 
-const includesRuby = (text: string) => {
-	return text.match(/<%[=\s]/);
+const includesRubyByEditorSelections = (editor: vscode.TextEditor) => {
+  return editor.selections.every(selection => {
+    const text = editor.document.getText(selection.with({ end: editor.document.lineAt(selection.end.line).range.end }));
+    return !text.includes('<%#') && (text.includes('<%') || text.includes('%>'));
+  });
 };
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const disposable = vscode.commands.registerTextEditorCommand('erb-toggle-comment.toggle', (editor, edit) => {
-		if (editor.selections.every(selection => !includesRuby(editor.document.getText(selection)))) {
-			vscode.commands.executeCommand('editor.action.commentLine');
-			return;
-		}
+  const disposable = vscode.commands.registerTextEditorCommand('erb-toggle-comment.toggle', (editor, edit) => {
+    if (!includesRubyByEditorSelections(editor)) {
+      vscode.commands.executeCommand('editor.action.commentLine');
+      return;
+    }
 
-		const endLine = editor.selection.end.character === 0 ? editor.selection.end.line - 1 : editor.selection.end.line;
-		const endPosition = editor.document.lineAt(endLine).range.end;
-		const newSelection = new Selection(new vscode.Position(editor.selection.start.line, 0), endPosition);
-		const indent = editor.document.lineAt(newSelection.start.line).firstNonWhitespaceCharacterIndex;
-		edit.replace(newSelection, toggleComment(editor.document.getText(newSelection), indent));
-	});
+    const endLine = editor.selection.end.character === 0 ? editor.selection.end.line - 1 : editor.selection.end.line;
+    const endPosition = editor.document.lineAt(endLine).range.end;
+    const newSelection = new Selection(new vscode.Position(editor.selection.start.line, 0), endPosition);
+    const indent = editor.document.lineAt(newSelection.start.line).firstNonWhitespaceCharacterIndex;
+    edit.replace(newSelection, toggleComment(editor.document.getText(newSelection), indent));
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
